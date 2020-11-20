a7e228f358a9bc6233cc7d984436643db069bf39
André Moreira <andremoreira93@gmail.com>
2020-11-19 22:47:25 -0300

    Updating

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 $(".fa-facebook").hover(function() {
-  $("#talk-box").text("Let's be friends.");
+  $("#talk-box").text("Vai lá me dar uma cutucada!");
 });
 $(".fa-twitter").hover(function() {
-  $("#talk-box").text("Send a tweet my way.");
+  $("#talk-box").text("Onde falo sozinho e espero que ninguém veja");
 });
 $(".fa-instagram").hover(function() {
-  $("#talk-box").text("Pictures from my travels.");
+  $("#talk-box").text("Troco likes! (É sério)");
 });
 $(".fa-tumblr").hover(function() {
   $("#talk-box").text("This is where I write things.");
 });
 $(".fa-linkedin").hover(function() {
-  $("#talk-box").text("Link up with me.");
+  $("#talk-box").text("Não vem de gracinha que aqui não é Tinder.");
 });
 $(".fa-youtube").hover(function() {
   $("#talk-box").text("Subscribe!");
 });
 $(".fa-github").hover(function() {
-  $("#talk-box").text("Git over here.");
+  $("#talk-box").text("Meus códigos TECNOLÓGICOS");
 });
 $(".fa-fire").hover(function() {
   $("#talk-box").text("Great green globs of greasy grimy gopher guts.");
 });
 $(".fa-facebook").hover(function() {
-  $("#talk-box").text("Let's be friends.");
+  $("#talk-box").text("Vai lá me dar uma cutucadinha ( ͡° ͜ʖ ͡°)");
 });
 $(".fa").mouseleave(function() {
   $("#talk-box").html("&nbsp;");
